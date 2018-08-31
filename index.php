b8610de7de1f94283401d9980b86efb287e605fb
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-08-31 16:33:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,14 +15,21 @@ if($method == 'POST')
 		$STATE= $json->queryResult->parameters->STATE;
 		$STATE= strtoupper($STATE);
 		if ($STATE == 'EVERY' or $STATE == 'ALL' or $STATE == 'EACH')
-		{$STATE = "";}
+		{
+		   $json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?command=$com";
+		}
+		else
+		{
+			$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?command=$com&STATE='$STATE'";
+		}
+			
 		
 		
 		
 		$username    = "SANYAM_K";
     		$password    = "Welcome@123";
 		
-		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?command=$com&STATE='$STATE'";
+		
 		
 		$ch      = curl_init( $json_url );
     		$options = array(
