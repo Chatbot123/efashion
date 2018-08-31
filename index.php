3d3f9504d78886435b9d8c9fb5a2428853374f1e
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-08-31 16:19:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if($method == 'POST')
 	$com = strtolower($com);
 	
 		
-	if ($com == 'amountsold') or ($com=='margin') or ($com=='qtysold')
+	if ($com == 'amountsold')
 	{
 		$STATE= $json->queryResult->parameters->STATE;
 		$STATE= strtoupper($STATE);
@@ -35,7 +35,79 @@ if($method == 'POST')
 		
 		foreach ($someobj["results"] as $value) 
 		{
-			$speech = "Total sale in ".$value["STATE"]." is $".$value["AMOUNT"];
+			$speech = "Total sale in ".$value["STATE"]." is $".$value["AMOUNT_SOLD"];
+			$speech .= "\r\n";
+			
+			
+       		 }
+			
+	
+		
+		
+	}
+	else if ($com == 'margin') 
+		
+	{
+		$STATE= $json->queryResult->parameters->STATE;
+		$STATE= strtoupper($STATE);
+		
+		
+		
+		$username    = "SANYAM_K";
+    		$password    = "Welcome@123";
+		
+		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?command=$com&STATE='$STATE'";
+		
+		$ch      = curl_init( $json_url );
+    		$options = array(
+        	CURLOPT_SSL_VERIFYPEER => false,
+        	CURLOPT_RETURNTRANSFER => true,
+        	CURLOPT_USERPWD        => "{$username}:{$password}",
+        	CURLOPT_HTTPHEADER     => array( "Accept: application/json" ),
+    		);
+    		curl_setopt_array( $ch, $options );
+		$json = curl_exec( $ch );
+		$someobj = json_decode($json,true);
+		
+		foreach ($someobj["results"] as $value) 
+		{
+			$speech = "Total sale in ".$value["STATE"]." is $".$value["MARGIN"];
+			$speech .= "\r\n";
+			
+			
+       		 }
+			
+	
+		
+		
+	}
+	else if ($com == 'qtysold') 
+		
+	{
+		$STATE= $json->queryResult->parameters->STATE;
+		$STATE= strtoupper($STATE);
+		
+		
+		
+		$username    = "SANYAM_K";
+    		$password    = "Welcome@123";
+		
+		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?command=$com&STATE='$STATE'";
+		
+		$ch      = curl_init( $json_url );
+    		$options = array(
+        	CURLOPT_SSL_VERIFYPEER => false,
+        	CURLOPT_RETURNTRANSFER => true,
+        	CURLOPT_USERPWD        => "{$username}:{$password}",
+        	CURLOPT_HTTPHEADER     => array( "Accept: application/json" ),
+    		);
+    		curl_setopt_array( $ch, $options );
+		$json = curl_exec( $ch );
+		$someobj = json_decode($json,true);
+		
+		foreach ($someobj["results"] as $value) 
+		{
+			$speech = "Total sale in ".$value["STATE"]." is $".$value["QUANTITY_SOLD"];
 			$speech .= "\r\n";
 			
 			
