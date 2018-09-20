a6637bd7d8cf4e5b2eb2f5ab6115b9a0f02da23e
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-20 20:25:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -292,9 +292,9 @@ if($method == 'POST')
 			$temp =  1.8*($file->main->temp - 273) +32 ;
 			$speech = "Now the Weather in $CITY is $weather_data , The temperature is $temp F " ;
 			$speech .= "\r\n";
-			/*$link = 'https://api.openweathermap.org/data/2.5/weather?q='.$CITY.'&appid=4b75f2eaa9f9a62fe7309f06b84b69f9'; // Link goes here!
+			$link = 'https://api.openweathermap.org/data/2.5/weather?q='.$CITY.'&appid=4b75f2eaa9f9a62fe7309f06b84b69f9'; // Link goes here!
 			//echo $link;
-			$speech = '<a href="'.$link.'">Link</a>';*/
+			$speech = '<a href="'.$link.'">Link</a>';
 			
 				
 			
