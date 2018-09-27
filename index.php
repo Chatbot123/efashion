0d5956e407ce5859f64950a2e0ed6f413ad900a3
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-27 14:31:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,62 +5,87 @@ if($method == 'POST')
 {
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-	$com = $json->queryResult->parameters->command;
+	if(isset($json->queryResult->parameters->command))
+		{	$com = $json->queryResult->parameters->command; } else {$com = '0';}
+	
 	$com = strtolower($com);
+	
 	if(isset($json->queryResult->parameters->myaction))
-		{	$my_action = $json->queryResult->parameters->myaction; } else {$my_action = "";}
+		{	$myaction = $json->queryResult->parameters->myaction; } else {$myaction = '0';}
 	
 	if(isset($json->queryResult->action))
-		{	$action = $json->queryResult->action; } else {$action = "";}
+		{	$action = $json->queryResult->action; } else {$action = '0';}
 	
+	if($action == 'MyPreviousIntent' and $myaction == 'HighLowValues' ){$action = 'HighLowValues';}
 	
-	if(($com == 'liststates' || $com == 'shoplist' || $com == 'listcity' || $com == 'listfamily' || $com == 'listcategory' || $com == 'listarticle' || $com == 'listyear') && $my_action == 'amountsold' && $action == '')
+	if(($com == 'liststates' || $com == 'shoplist' || $com == 'listcity' || $com == 'listfamily' || $com == 'listcategory' || $com == 'listarticle' || $com == 'listyear') && $my_action == 'amountsold' && $action == '0')
 	{$com = "amountsold";}
 	
-	if(($com == 'liststates' || $com == 'shoplist' || $com == 'listcity' || $com == 'listfamily' || $com == 'listcategory' || $com == 'listarticle' || $com == 'listyear') && $my_action == 'qtysold' && $action == '' )
+	if(($com == 'liststates' || $com == 'shoplist' || $com == 'listcity' || $com == 'listfamily' || $com == 'listcategory' || $com == 'listarticle' || $com == 'listyear') && $my_action == 'qtysold' && $action == '0' )
 	{$com = "qtysold";}
 	
-	if(($com == 'liststates' || $com == 'shoplist' || $com == 'listcity' || $com == 'listfamily' || $com == 'listcategory' || $com == 'listarticle' || $com == 'listyear') && $my_action == 'margin' && $action == '')
+	if(($com == 'liststates' || $com == 'shoplist' || $com == 'listcity' || $com == 'listfamily' || $com == 'listcategory' || $com == 'listarticle' || $com == 'listyear') && $my_action == 'margin' && $action == '0')
 	{$com = "margin";}
 	
+	//to execute xsjs for high and low measures
+	$xsjs_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?";
 		
 	
 		if(isset($json->queryResult->parameters->STATE))
-		{	$STATE= $json->queryResult->parameters->STATE; } else {$STATE = '0';}
-		
+		{	$STATE= $json->queryResult->parameters->STATE; } 
+	
+		if(isset($json->queryResult->parameters->ENT_STATE))
+		{	$ENT_STATE= $json->queryResult->parameters->ENT_STATE;	}
+	
 		if(isset($json->queryResult->parameters->CITY))
-		{	$CITY= $json->queryResult->parameters->CITY; } else {$CITY = '0';}
-				
+		{	$CITY= $json->queryResult->parameters->CITY; } 
+	
+		if(isset($json->queryResult->parameters->ENT_CITY))
+		{	$ENT_CITY= $json->queryResult->parameters->ENT_CITY; } 
+	
 		if(isset($json->queryResult->parameters->SHOPNAME))
-		{	$SHOPNAME= $json->queryResult->parameters->SHOPNAME; } else {$SHOPNAME = '0';}
+		{	$SHOPNAME= $json->queryResult->parameters->SHOPNAME; }
+	
+		if(isset($json->queryResult->parameters->ENT_SHOP))
+		{	$ENT_SHOP= $json->queryResult->parameters->ENT_SHOP; }
 	
 		if(isset($json->queryResult->parameters->YR))
-		{	$YR= $json->queryResult->parameters->YR; } else {$YR = '0';}
+		{	$YR= $json->queryResult->parameters->YR; } 
 		
 		if(isset($json->queryResult->parameters->QTR))
-		{	$QTR= $json->queryResult->parameters->QTR; } else {$QTR = '0';}
+		{	$QTR= $json->queryResult->parameters->QTR; }
 		
 		if(isset($json->queryResult->parameters->MTH))
-		{	$MTH= $json->queryResult->parameters->MTH; } else {$MTH = '0';}	
+		{	$MTH= $json->queryResult->parameters->MTH; } 
 
 	   	if(isset($json->queryResult->parameters->FAMILY))
-		{	$FAMILY= $json->queryResult->parameters->FAMILY; } else {$FAMILY = '0';}
+		{	$FAMILY= $json->queryResult->parameters->FAMILY; } 
+	
+		if(isset($json->queryResult->parameters->ENT_FAM))
+		{	$ENT_FAM= $json->queryResult->parameters->ENT_FAM; } 
 	
 	     	if(isset($json->queryResult->parameters->CATEGORY))
-		{	$CATEGORY= $json->queryResult->parameters->CATEGORY; } else {$CATEGORY = '0';}
+		{	$CATEGORY= $json->queryResult->parameters->CATEGORY; } 
+	
+		if(isset($json->queryResult->parameters->ENT_CAT))
+		{	$ENT_CAT= $json->queryResult->parameters->ENT_CAT; }
 	
 	     	if(isset($json->queryResult->parameters->ARTICLE))
-		{	$ARTICLE= $json->queryResult->parameters->ARTICLE; } else {$ARTICLE = '0';}
+		{	$ARTICLE= $json->queryResult->parameters->ARTICLE; } 
 	
+		if(isset($json->queryResult->parameters->ENT_ARTICLE))
+		{	$ENT_ARTICLE= $json->queryResult->parameters->ENT_ARTICLE; }
+	
+		if(isset($json->queryResult->parameters->NUM))
+		{	$NUM= $json->queryResult->parameters->NUM;
+			
+		}
+
+		if(isset($json->queryResult->parameters->ENT_TOP_BOT))
+		{	$ENT_TOP_BOT= $json->queryResult->parameters->ENT_TOP_BOT; } 
 		
-			/*$my_previous_com = $com;
-		
-		if(isset($json->queryResult->parameters->myaction))
-		{	$my_action = $json->queryResult->parameters->myaction; } else {$my_action = "";}
-		
-		/*if($my_action == 'PreviousContextTotSale'){$com = $my_previous_com;}
-		if($my_action == 'PreviousContextQtySold'){$com = $my_previous_com;}
-		if($my_action == 'PreviousContextMargin'){$com = $my_previous_com;}*/
+		if(isset($json->queryResult->parameters->ENT_MEASURE))
+		{	$ENT_MEASURE= $json->queryResult->parameters->ENT_MEASURE; }
 	
 		$CITY= strtoupper($CITY);
 		$STATE= strtoupper($STATE);
@@ -71,9 +96,12 @@ if($method == 'POST')
 		$YR= strtoupper($YR);
 		$MTH= strtoupper($MTH);
 		$QTR= strtoupper($QTR);
+		$ENT_MEASURE= strtoupper($ENT_MEASURE);
+		$ENT_TOP_BOT= strtoupper($ENT_TOP_BOT);
+	
 	
 		$SHOPNAME = str_replace(' ', '', $SHOPNAME);
-		
+		$CITY = str_replace(' ', '', $CITY);
 		$STATE = str_replace(' ', '', $STATE);
 		$FAMILY = str_replace(' ', '', $FAMILY);
 		$CATEGORY = str_replace(' ', '', $CATEGORY);
@@ -82,31 +110,33 @@ if($method == 'POST')
 		$MTH = str_replace(' ', '', $MTH);
 		$QTR = str_replace(' ', '', $QTR);
 		
-		if($CITY=="" ){	$CITY='0';}
-		if($STATE=="" ){$STATE='0';}
-		if($SHOPNAME=="" ){$SHOPNAME='0';}
-		if($FAMILY=="" ){$FAMILY='0';}
-		if($CATEGORY=="" ){$CATEGORY='0';}
-		if($ARTICLE=="" ){$ARTICLE='0';}
-		if($YR=="" ){$YR='0';}
-		if($MTH=="" ){$MTH='0';}
-		if($QTR=="" ){$QTR='0';}
+		$qty_array = array("QUANTITY","QTY","ITEMS","PRODUCTS");
+		if (in_array($ENT_MEASURE, $qty_array)) {$showqty=1;}
+	
 		
+	
 		
-		
-		$userespnose = array("PLEASEIGNORE", "IGNORE","IGNOREIT", "ANYVALUE","ANY","NOIDEA");
+	
+		/*$sale_array = array("SALES","SALE");
+		if (in_array($ENT_MEASURE, $sale_array)) {$showsale=1;}
+		$margin_array = array("MARGIN","PROFIT");
+		if (in_array($ENT_MEASURE, $margin_array)) {$showmearuse=1;}
+		$bottom_array =  array("LOWEST","MINIMUM");
+		if (in_array($ENT_TOP_BOT, $bottom_array)) {$NUM=1;}*/
+	
+		$userespnose = array("PLEASEIGNORE", "IGNORE","IGNOREIT", "ANYVALUE","ANY","NOIDEA","DRILLUP");
 		
 		if (in_array($YR, $userespnose)) {$YR='0';}
 		if (in_array($QTR, $userespnose)) {$QTR='0';}
 		if (in_array($MTH, $userespnose)) {$MTH='0';}
 	
-		$useres = array("PLEASEIGNORE", "IGNORE","IGNOREIT");
-		if (in_array($STATE, $useres)) {$STATE='0';}
-		if (in_array($CITY, $useres)) {$CITY='0';}
-		if (in_array($SHOPNAME, $useres)) {$SHOPNAME='0';}
-		if (in_array($FAMILY, $useres)) {$FAMILY='0';}
-		if (in_array($CATEGORY, $useres)) {$CATEGORY='0';}
-		if (in_array($ARTICLE, $useres)) {$ARTICLE='0';}    
+		$useres = array("PLEASEIGNORE", "IGNORE","IGNOREIT", "DRILLUP");
+		if (in_array($STATE, $useres)) {$STATE='0'; $ENT_STATE ="";}
+		if (in_array($CITY, $useres)) {$CITY='0'; $ENT_CITY ="";}
+		if (in_array($SHOPNAME, $useres)) {$SHOPNAME='0'; $ENT_SHOP ="";}
+		if (in_array($FAMILY, $useres)) {$FAMILY='0'; $ENT_FAM ="";}
+		if (in_array($CATEGORY, $useres)) {$CATEGORY='0'; $ENT_CAT ="";}
+		if (in_array($ARTICLE, $useres)) {$ARTICLE='0'; $ENT_ARTICLE ="";}    
 		    
 		$userespnose = array("EACH", "EVERY","ALL");
 		if(in_array($STATE, $userespnose))
@@ -145,7 +175,70 @@ if($method == 'POST')
 		{
 			$ARTICLE = 'ALL';
 		}
-		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_DEV.xsjs?command=$com&STATE=$STATE&CITY=$CITY&SHOPNAME=$SHOPNAME&YR=$YR&QTR=$QTR&MTH=$MTH&FAMILY=$FAMILY&CATEGORY=$CATEGORY&ARTICLE=$ARTICLE";		
+	
+		if($CITY=="" )		{	$CITY='0';  }
+		if($STATE=="" )		{	$STATE='0'; }
+		if($SHOPNAME=="" )	{	$SHOPNAME='0'; 	}
+		if($FAMILY=="" )	{	$FAMILY='0'; 	}
+		if($CATEGORY=="" )	{	$CATEGORY='0'; 	}
+		if($ARTICLE=="" )	{	$ARTICLE='0'; 	}
+		if($YR=="" )		{	$YR='0'; 	}
+		if($MTH=="" )		{	$MTH='0';	}
+		if($QTR=="" )		{	$QTR='0'; 	}
+		
+		if($ENT_CITY=="" )	{	$ENT_CITY='0';	} else { $xsjs_url .= "&ENT_CITY=$ENT_CITY"; }
+		if($ENT_STATE=="" )	{	$ENT_STATE='0'; } else { $xsjs_url .= "&ENT_STATE=$ENT_STATE"; }
+		if($ENT_SHOP=="" )	{	$ENT_SHOP='0';	} else { $xsjs_url .= "&ENT_SHOP=$ENT_SHOP"; }
+		if($ENT_FAM=="" )	{	$ENT_FAM='0';	} else { $xsjs_url .= "&ENT_FAM=$ENT_FAM"; }
+		if($ENT_CAT=="" )	{	$ENT_CAT='0';	} else { $xsjs_url .= "&ENT_CAT=$ENT_CAT"; }
+		if($ENT_ARTICLE=="" )	{	$ENT_ARTICLE='0'; } else { $xsjs_url .= "&ENT_ARTICLE=$ENT_ARTICLE"; }
+		if($ENT_TOP_BOT=="" )	{	$ENT_TOP_BOT='0'; } else { $xsjs_url .= "&ENT_TOP_BOT=$ENT_TOP_BOT"; }
+		if($ENT_MEASURE=="" )	{	$ENT_MEASURE='0'; } else { $xsjs_url .= "&ENT_MEASURE=$ENT_MEASURE";}
+		if($NUM == "") 		{	$NUM='0'; } 
+		$top_array =  array("HIGHEST","MAXIMUM","LOWEST","MINIMUM");
+		if (in_array($ENT_TOP_BOT, $top_array)) 
+		{
+			$NUM=1;
+			$disnum = "";
+			$disval = "VALUE IS ";
+		}
+		else 
+		{
+			$disnum=$NUM;
+			$disval = "VALUES ARE ";
+		}
+		$xsjs_url .= "&CITY=$CITY";
+		
+		
+		$xsjs_url .= "&STATE=$STATE";
+		$xsjs_url .= "&SHOPNAME=$SHOPNAME";
+		$xsjs_url .= "&FAMILY=$FAMILY";
+		$xsjs_url .= "&CATEGORY=$CATEGORY";
+		$xsjs_url .= "&ARTICLE=$ARTICLE";
+		$xsjs_url .= "&YR=$YR";
+		$xsjs_url .= "&MTH=$MTH";
+		$xsjs_url .= "&QTR=$QTR";
+		$xsjs_url .= "&NUM=$NUM";
+	//echo $xsjs_url;
+		if($action == 'HighLowValues')
+		{
+			$username    = "SANYAM_K";
+			$password    = "Welcome@123";
+			$ch      = curl_init( $xsjs_url );
+			$options = array(
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_USERPWD        => "{$username}:{$password}",
+			CURLOPT_HTTPHEADER     => array( "Accept: application/json" ),
+			);
+			curl_setopt_array( $ch, $options );
+			$json = curl_exec( $ch );
+			$someobj = json_decode($json,true);
+		}
+		else
+		{
+		
+		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_DEV.xsjs?command=$com&STATE=$STATE&CITY=$CITY&SHOPNAME=$SHOPNAME&YR=$YR&QTR=$QTR&MTH=$MTH&FAMILY=$FAMILY&CATEGORY=$CATEGORY&ARTICLE=$ARTICLE&ACTION=$action";		
 		
 		$username    = "SANYAM_K";
     		$password    = "Welcome@123";
@@ -159,27 +252,54 @@ if($method == 'POST')
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
 		$someobj = json_decode($json,true);
-		if($com == 'amountsold' or $com == 'margin' or $com == 'qtysold')
+		}
+	//echo $json_url;
+		if($com == 'amountsold' or $com == 'margin' or $com == 'qtysold' or $action == 'HighLowValues')
 		{
-			$CITY = str_replace(' ', '', $CITY);
-			if ($com == 'amountsold')
-				$distext = "Total sale value is of worth $";
-			else if($com == 'margin')
-				$distext = "Total profit value is of worth $";
-			else if ($com == 'qtysold')
-				$distext = "Total ";
-			if($CITY !='0')	{ $discity = " for city "; } else { $discity = ""; }
-			if($STATE !='0'){ $disstate = " in state "; } else { $disstate = ""; }
-			if($FAMILY !='0'){ $disfamily = " family of product sold "; } else {$disfamily = ""; }
-            		if($CATEGORY !='0'){ $discategory = " category sold "; }	else { $discategory = ""; }
-            		if($ARTICLE !='0'){$disarticle = " article sold ";} else	{ $disarticle = ""; }
-			if($SHOPNAME != '0') { $disshop = " of shop "; } else{	$disshop = "";	}
+			
+			if ($com == 'amountsold' )
+			{
+				$distext = "Sale  ";
+				$distext .= "\r\n";
+				$show_dlr = "worth of $";
+			}
+			else if($com == 'margin' )
+			{
+				$distext = "Margin ";
+				$distext .= "\r\n";
+				$show_dlr = "worth of $";
+			}
+			else if ($com == 'qtysold' )
+			{
+				$distext = "Products ";
+				$distext .= "\r\n";
+				$show_dlr = "";
+			}
+			if ($action == 'HighLowValues')
+			{
+				
+				$distext = "$ENT_TOP_BOT $disnum $ENT_MEASURE $disval ";
+				if($showqty==1)
+				{$show_dlr = "";} else {$show_dlr = "Worth of $";}
+				 $distext .= "\r\n";
+				$speech .= $distext;
+				
+			}
+			if($CITY !='0' 	|| $ENT_CITY != '0')	{ $discity = " for city "; } else { $discity = ""; }
+			if($STATE !='0' || $ENT_STATE != '0'){ $disstate = " in state "; } else { $disstate = ""; }
+			if($FAMILY !='0' || $ENT_FAM != '0'){ $disfamily = " family of product sold "; } else {$disfamily = ""; }
+            		if($CATEGORY !='0' || $ENT_CAT != '0'){ $discategory = " category sold "; }	else { $discategory = ""; }
+            		if($ARTICLE !='0' || $ENT_ARTICLE != '0'){$disarticle = " article sold ";} else	{ $disarticle = ""; }
+			if($SHOPNAME != '0' || $ENT_SHOP != '0') { $disshop = " of shop "; } else{	$disshop = "";	}
 			if($YR != '0')	{      $disyear = " for year ";} else {$disyear = "";}
 			if($QTR != '0')	{      $disqtr = " in quarter ";} else {$disqtr = "";}
 			if($MTH != '0')	{      $dismth = " for month ";} else {$dismth = "";}
+			
+			
+			
 			foreach ($someobj["results"] as $value) 
 			{
-				$speech .= $distext. $value["AMOUNT"].$disshop.$value["SHOP_NAME"].$discity.$value["CITY"].$disstate.$value["STATE"]." ".$value["FAMILY_NAME"].$disfamily." ".$value["CATEGORY"].$discategory." ".$value["ARTICLE_LABEL"].$disarticle.$disqtr.$value["QTR"].$dismth.$value["MTH"].$disyear.$value["YR"];
+				$speech .=  $distext.$show_dlr. $value["AMOUNT"].$disshop.$value["SHOP_NAME"].$discity.$value["CITY"].$disstate.$value["STATE"]." ".$value["FAMILY_NAME"].$disfamily." ".$value["CATEGORY"].$discategory." ".$value["ARTICLE_LABEL"].$disarticle.$disqtr.$value["QTR"].$dismth.$value["MTH"].$disyear.$value["YR"];
 				$speech .= "\r\n";
 				//$speech .= "Do you want this info on mail\n";
 			 }
@@ -274,7 +394,8 @@ if($method == 'POST')
 	
 	else if ($com=='weather')
 	{
-			
+			if(isset($json->queryResult->parameters->CITY))
+		{	$CITY= $json->queryResult->parameters->CITY; } 
 		if(strlen($CITY) > 1) 
 		{	 
 
@@ -297,7 +418,6 @@ if($method == 'POST')
 			$speech = "Now the Weather in $CITY can be seen at below link";
 			$speech .= "\r\n";
 			$link = "https://www.timeanddate.com/weather/usa/".$CITY;
-			//echo $link;
 			$speech .= $link;
 			
 				
