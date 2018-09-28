11f416111572343d84c60e7bbd043215aca2218b
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-28 16:55:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if($method == 'POST')
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	if(isset($json->queryResult->parameters->command))
-		{	$com = $json->queryResult->parameters->command; } else {$com = '0';}
+		{	$com = $json->queryResult->parameters->command; } else {$com = "";}
 	
 	$com = strtolower($com);
 	
@@ -28,7 +28,7 @@ if($method == 'POST')
 	{$com = "margin";}
 	
 	//to execute xsjs for high and low measures
-	$xsjs_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST.xsjs?";
+	$xsjs_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_DEV_TOP.xsjs?";
 		
 	
 		if(isset($json->queryResult->parameters->STATE))
@@ -131,69 +131,77 @@ if($method == 'POST')
 		if (in_array($MTH, $userespnose)) {$MTH='0';}
 	
 		$useres = array("PLEASEIGNORE", "IGNORE","IGNOREIT", "DRILLUP");
-		if (in_array($STATE, $useres)) {$STATE='0'; $ENT_STATE ="";}
-		if (in_array($CITY, $useres)) {$CITY='0'; $ENT_CITY ="";}
-		if (in_array($SHOPNAME, $useres)) {$SHOPNAME='0'; $ENT_SHOP ="";}
-		if (in_array($FAMILY, $useres)) {$FAMILY='0'; $ENT_FAM ="";}
-		if (in_array($CATEGORY, $useres)) {$CATEGORY='0'; $ENT_CAT ="";}
-		if (in_array($ARTICLE, $useres)) {$ARTICLE='0'; $ENT_ARTICLE ="";}    
+		if (in_array($STATE, $useres)) {$STATE=""; $ENT_STATE ="";}
+		if (in_array($CITY, $useres)) {$CITY=""; $ENT_CITY ="";}
+		if (in_array($SHOPNAME, $useres)) {$SHOPNAME=""; $ENT_SHOP ="";}
+		if (in_array($FAMILY, $useres)) {$FAMILY=""; $ENT_FAM ="";}
+		if (in_array($CATEGORY, $useres)) {$CATEGORY=""; $ENT_CAT ="";}
+		if (in_array($ARTICLE, $useres)) {$ARTICLE=""; $ENT_ARTICLE ="";}    
 		    
 		$userespnose = array("EACH", "EVERY","ALL");
-		if(in_array($STATE, $userespnose))
-		{
-			$STATE = 'ALL';
+		if(in_array($YR, $userespnose))	{ $YR = 'ALL';	}
+		if(in_array($QTR, $userespnose)){ $QTR = 'ALL';	}
+		if(in_array($MTH, $userespnose)){ $MTH = 'ALL';	}
+		if(in_array($CITY, $userespnose)){ $CITY = 'ALL'; }
+		if(in_array($FAMILY, $userespnose)){ $FAMILY = 'ALL'; }
+		if(in_array($CATEGORY, $userespnose)){	$CATEGORY = 'ALL';}
+		if(in_array($ARTICLE, $userespnose)){	$ARTICLE = 'ALL'; }
+		if(in_array($STATE, $userespnose)){	$STATE = 'ALL'; }
+		if(in_array($SHOPNAME, $userespnose)){	$SHOPNAME = 'ALL'; }
+	
+		if($CITY != "" and $CITY != 'ALL' )
+		{ 
+			$xsjs_url .= "&CITY=$CITY"; 
+			$ENT_CITY = "city";
 		}
 	
-		if(in_array($SHOPNAME, $userespnose))
+		if($STATE!="" and $STATE !='ALL' )
+		 { 
+			$xsjs_url .= "&STATE=$STATE";
+		  	$ENT_STATE = "state";
+		 }
+	
+		if($SHOPNAME!="" and $SHOPNAME != 'ALL' )
 		{
-			$SHOPNAME = 'ALL';
-		}
-		
-		if(in_array($CITY, $userespnose))
-		{
-			$CITY = 'ALL';
-		}
-		
-		if(in_array($YR, $userespnose))
-		{
-			$YR = 'ALL';
-		}
-		
-		if(in_array($FAMILY, $userespnose))
-		{
-			$FAMILY = 'ALL';
-		}
-		
-		
-		if(in_array($CATEGORY, $userespnose))
-		{
-			$CATEGORY = 'ALL';
-		}
-		
-		
-		if(in_array($ARTICLE, $userespnose))
-		{
-			$ARTICLE = 'ALL';
+			$xsjs_url .= "&SHOPNAME=$SHOPNAME"; 
+			$ENT_SHOP = "shop";
 		}
 	
-		if($CITY=="" )		{	$CITY='0';  }
-		if($STATE=="" )		{	$STATE='0'; }
-		if($SHOPNAME=="" )	{	$SHOPNAME='0'; 	}
-		if($FAMILY=="" )	{	$FAMILY='0'; 	}
-		if($CATEGORY=="" )	{	$CATEGORY='0'; 	}
-		if($ARTICLE=="" )	{	$ARTICLE='0'; 	}
+		if($FAMILY!="" and $FAMILY != 'ALL' )
+		{ 
+			$xsjs_url .= "&FAMILY=$FAMILY";
+			$ENT_FAM = "family";
+		}
+	
+		if($CATEGORY!="" and $CATEGORY != 'ALL' )
+		{ 
+			$xsjs_url .= "&CATEGORY=$CATEGORY"; 
+			$ENT_CAT = "category";
+		}
+	
+		if($ARTICLE!="" and $ARTICLE != 'ALL')	
+		 { 
+			$xsjs_url .= "&ARTICLE=$ARTICLE"; 
+		 	$ENT_ARTICLE = "article";
+		 }
+		if($com!="")	
+		 { 
+			$xsjs_url .= "&COMMAND=$com";
+		 	
+		 }
+	
 		if($YR=="" )		{	$YR='0'; 	}
 		if($MTH=="" )		{	$MTH='0';	}
 		if($QTR=="" )		{	$QTR='0'; 	}
-		
-		if($ENT_CITY=="" )	{	$ENT_CITY='0';	} else { $xsjs_url .= "&ENT_CITY=$ENT_CITY"; }
-		if($ENT_STATE=="" )	{	$ENT_STATE='0'; } else { $xsjs_url .= "&ENT_STATE=$ENT_STATE"; }
-		if($ENT_SHOP=="" )	{	$ENT_SHOP='0';	} else { $xsjs_url .= "&ENT_SHOP=$ENT_SHOP"; }
-		if($ENT_FAM=="" )	{	$ENT_FAM='0';	} else { $xsjs_url .= "&ENT_FAM=$ENT_FAM"; }
-		if($ENT_CAT=="" )	{	$ENT_CAT='0';	} else { $xsjs_url .= "&ENT_CAT=$ENT_CAT"; }
-		if($ENT_ARTICLE=="" )	{	$ENT_ARTICLE='0'; } else { $xsjs_url .= "&ENT_ARTICLE=$ENT_ARTICLE"; }
-		if($ENT_TOP_BOT=="" )	{	$ENT_TOP_BOT='0'; } else { $xsjs_url .= "&ENT_TOP_BOT=$ENT_TOP_BOT"; }
-		if($ENT_MEASURE=="" )	{	$ENT_MEASURE='0'; } else { $xsjs_url .= "&ENT_MEASURE=$ENT_MEASURE";}
+		if($action!="" )	 { $xsjs_url .= "&ACTION=$action"; }
+		if($ENT_CITY!="" )	 { $xsjs_url .= "&ENT_CITY=$ENT_CITY"; }
+		if($ENT_STATE!="" )	 { $xsjs_url .= "&ENT_STATE=$ENT_STATE"; }
+		if($ENT_SHOP!="" )	 { $xsjs_url .= "&ENT_SHOP=$ENT_SHOP"; }
+		if($ENT_FAM!="")	{ $xsjs_url .= "&ENT_FAM=$ENT_FAM"; }
+		if($ENT_CAT!="")	{ $xsjs_url .= "&ENT_CAT=$ENT_CAT"; }
+		if($ENT_ARTICLE!="" )	{ $xsjs_url .= "&ENT_ARTICLE=$ENT_ARTICLE"; }
+		if($ENT_TOP_BOT!="")	{ $xsjs_url .= "&ENT_TOP_BOT=$ENT_TOP_BOT"; }
+		if($ENT_MEASURE!="" )	 { $xsjs_url .= "&ENT_MEASURE=$ENT_MEASURE";}
 		if($NUM == "") 		{	$NUM='0'; } 
 		$top_array =  array("HIGHEST","MAXIMUM","LOWEST","MINIMUM");
 		if (in_array($ENT_TOP_BOT, $top_array)) 
@@ -207,21 +215,22 @@ if($method == 'POST')
 			$disnum=$NUM;
 			$disval = "VALUES ARE ";
 		}
-		$xsjs_url .= "&CITY=$CITY";
+		/*$xsjs_url .= "&CITY=$CITY";
 		
 		
 		$xsjs_url .= "&STATE=$STATE";
 		$xsjs_url .= "&SHOPNAME=$SHOPNAME";
 		$xsjs_url .= "&FAMILY=$FAMILY";
 		$xsjs_url .= "&CATEGORY=$CATEGORY";
-		$xsjs_url .= "&ARTICLE=$ARTICLE";
+		$xsjs_url .= "&ARTICLE=$ARTICLE";*/
 		$xsjs_url .= "&YR=$YR";
 		$xsjs_url .= "&MTH=$MTH";
 		$xsjs_url .= "&QTR=$QTR";
 		$xsjs_url .= "&NUM=$NUM";
+		
 	//echo $xsjs_url;
-		if($action == 'HighLowValues')
-		{
+		//if($action == 'HighLowValues')
+		//{
 			$username    = "SANYAM_K";
 			$password    = "Welcome@123";
 			$ch      = curl_init( $xsjs_url );
@@ -234,7 +243,7 @@ if($method == 'POST')
 			curl_setopt_array( $ch, $options );
 			$json = curl_exec( $ch );
 			$someobj = json_decode($json,true);
-		}
+		/*}
 		else
 		{
 		
@@ -252,27 +261,27 @@ if($method == 'POST')
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
 		$someobj = json_decode($json,true);
-		}
+		}*/
 	//echo $json_url;
-		if($com == 'amountsold' or $com == 'margin' or $com == 'qtysold' or $action == 'HighLowValues')
+		if($com == 'amountsold' or $com == 'margin' or $com == 'qtysold' or $action == 'HighLowValues'  )
 		{
 			
 			if ($com == 'amountsold' )
 			{
 				$distext = "Sale  ";
-				//$distext .= "\r\n";
+				$distext .= "\r\n";
 				$show_dlr = "worth of $";
 			}
 			else if($com == 'margin' )
 			{
 				$distext = "Margin ";
-				//$distext .= "\r\n";
+				$distext .= "\r\n";
 				$show_dlr = "worth of $";
 			}
 			else if ($com == 'qtysold' )
 			{
 				$distext = "Products ";
-				//$distext .= "\r\n";
+				$distext .= "\r\n";
 				$show_dlr = "";
 			}
 			if ($action == 'HighLowValues')
@@ -282,15 +291,16 @@ if($method == 'POST')
 				if($showqty==1)
 				{$show_dlr = "";} else {$show_dlr = "Worth of $";}
 				 $distext .= "\r\n";
-				 $speech .= $distext;
-				 $distext="";
+				$speech .= $distext;
+				$distext="";
+				
 			}
-			if($CITY !='0' 	|| $ENT_CITY != '0')	{ $discity = " for city "; } else { $discity = ""; }
-			if($STATE !='0' || $ENT_STATE != '0'){ $disstate = " in state "; } else { $disstate = ""; }
-			if($FAMILY !='0' || $ENT_FAM != '0'){ $disfamily = " family of product sold "; } else {$disfamily = ""; }
-            		if($CATEGORY !='0' || $ENT_CAT != '0'){ $discategory = " category sold "; }	else { $discategory = ""; }
-            		if($ARTICLE !='0' || $ENT_ARTICLE != '0'){$disarticle = " article sold ";} else	{ $disarticle = ""; }
-			if($SHOPNAME != '0' || $ENT_SHOP != '0') { $disshop = " of shop "; } else{	$disshop = "";	}
+			if($CITY !="" 	|| $ENT_CITY !="")	{ $discity = " for city "; } else { $discity = ""; }
+			if($STATE !="" || $ENT_STATE !=""){ $disstate = " in state "; } else { $disstate = ""; }
+			if($FAMILY !="" || $ENT_FAM !=""){ $disfamily = " family of product sold "; } else {$disfamily = ""; }
+            		if($CATEGORY !="" || $ENT_CAT !=""){ $discategory = " category sold "; }	else { $discategory = ""; }
+            		if($ARTICLE !="" || $ENT_ARTICLE !=""){$disarticle = " article sold ";} else	{ $disarticle = ""; }
+			if($SHOPNAME !="" || $ENT_SHOP !="") { $disshop = " of shop "; } else{	$disshop = "";	}
 			if($YR != '0')	{      $disyear = " for year ";} else {$disyear = "";}
 			if($QTR != '0')	{      $disqtr = " in quarter ";} else {$disqtr = "";}
 			if($MTH != '0')	{      $dismth = " for month ";} else {$dismth = "";}
