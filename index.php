40db5dd8b1362adc4ad91515926073783a9c3f5a
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-01-18 12:35:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -253,7 +253,7 @@ if($method == 'POST')
 		$xsjs_url .= "&QTR=$QTR";
 		$xsjs_url .= "&NUM=$NUM";
 		
-	echo $xsjs_url;
+	//echo $xsjs_url;
 		
 			$username    = "SANYAM_K";
 			$password    = "Welcome@234";
