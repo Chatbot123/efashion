96275e5948601ce15f8d5ff294e8f509397d18ca
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-08-02 14:25:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -256,7 +256,7 @@ if($method == 'POST')
 	//echo $xsjs_url;
 		
 			$username    = "SANYAM_K";
-			$password    = "Welcome@234";
+			$password    = "Welcome@2019";
 			$ch      = curl_init( $xsjs_url );
 			$options = array(
 			CURLOPT_SSL_VERIFYPEER => false,
